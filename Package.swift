890ed13b02b524d4e0083e798ed47e58feabc5f6
Eugene Antropov <antigp@gmail.com>
2020-06-21 22:14:40 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
 import PackageDescription
 
 let package = Package(
@@ -9,7 +9,8 @@ let package = Package(
   targets: [
     .target(
       name: "CocoaImageHashing",
-      path: "CocoaImageHashing"
+      path: "CocoaImageHashing",
+      sources: ["."]
     )
   ]
 )
